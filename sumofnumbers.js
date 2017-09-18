25976df48218ef137858ae20702d73e906fe1f16
robertatienza <31534223+robertatienza@users.noreply.github.com>
2017-09-17 23:14:16 -1000

    done E26

---
+++ sumofnumbers.js
@@ -0,0 +1,44 @@
+/**
+ * E26
+ */
+
+const testList = [1, 2, 3, 4, 5];
+
+function sumFor(nums) {
+  let total = 0;
+  for (const num of nums) {
+    total += num;
+  }
+
+  return total;
+}
+
+console.log(sumFor(testList));
+
+function sumWhile(nums) {
+  let total = 0;
+  let i = 0;
+  while (i < nums.length) {
+    total += nums[i];
+    i++;
+  }
+  return total;
+}
+
+console.log(sumWhile(testList));
+
+function sumRecursion(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  return nums[0] + sumRecursion(nums.slice(1, nums.length));
+}
+
+console.log(sumRecursion(testList));
+
+
+function sumTheSimpleWay(nums) {
+  return _.reduce(nums, function (memo, val) { return memo + val; }, 0);
+}
+
+console.log(sumTheSimpleWay(testList));
